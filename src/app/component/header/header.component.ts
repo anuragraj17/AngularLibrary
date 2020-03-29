977a7db7851928428a8b5df7335491925eefd50f
anuragraj17 <anuragsknsits@gmail.com>
2020-03-29 20:05:37 +0530

    discard changes in front header for login logout.

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -7,21 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  userName : String;
-  isUserLogIn : boolean;
-
   constructor() { }
 
   ngOnInit() {
-    this.userName = window.localStorage.getItem("userName");
-    this.isUserLogIn = this.userName != null; 
-    console.log(this.isUserLogIn);
   }
-
-  logOutAuthUser() {
-    window.localStorage.removeItem("userName");
-    this.userName = null;
-    this.isUserLogIn  = false;
- }
-
 }
